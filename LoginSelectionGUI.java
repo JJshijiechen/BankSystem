ee8ff3970a0fc9a5873c85e4c34fc7c91b04837e
Yinghui Du <101728776+dyhhhhhhhhh@users.noreply.github.com>
2024-08-13 00:30:47 +0800

    Update LoginSelectionGUI.java

--- LoginSelectionGUI.java
+++ LoginSelectionGUI.java
@@ -1,5 +1,46 @@
-package BankSystem;
+import java.awt.EventQueue;
 
-public class LoginSelectionGUI {
-    
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+class LoginSelectionGUI extends JFrame {
+    private JButton userLoginButton, adminLoginButton;
+
+    public LoginSelectionGUI() {
+        setTitle("Select Login Type");
+        setSize(300, 200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        JPanel panel = new JPanel();
+        add(panel);
+        placeComponents(panel);
+
+        setVisible(true);
+    }
+
+    private void placeComponents(JPanel panel) {
+        panel.setLayout(null);
+
+        userLoginButton = new JButton("User Login");
+        userLoginButton.setBounds(50, 50, 100, 50);
+        panel.add(userLoginButton);
+
+        adminLoginButton = new JButton("Admin Login");
+        adminLoginButton.setBounds(160, 50, 100, 50);
+        panel.add(adminLoginButton);
+
+        userLoginButton.addActionListener(e -> {
+            new LoginGUI();
+            dispose();
+        });
+
+        adminLoginButton.addActionListener(e -> {
+            new AdminLoginGUI();
+            dispose();
+        });
+    }
 }
+
