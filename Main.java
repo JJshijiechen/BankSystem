6e82ddc3ac71c95f37fe70276545c67a1c9dd793
Shijie Chen (Jason) <sc8722@nyu.edu>
2024-08-12 12:23:20 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,10 @@
-package BankSystem;
+import javax.swing.*;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        // Launch the login selection GUI
+        SwingUtilities.invokeLater(() -> {
+            new LoginSelectionGUI();
+        });
+    }
 }
